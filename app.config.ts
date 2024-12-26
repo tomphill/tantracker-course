551634b48ae9f7ad74fd42dadd2cbb5ac281d763
tom <tomphill@live.co.uk>
2024-12-26 11:15:32 +0800

    server preset

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@tanstack/start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    preset: "vercel",
+  },
   vite: {
     plugins: [
       // this is the plugin that enables path aliases
